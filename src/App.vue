03d3423dcda4ef1bd0fad971e259c05ef70f63eb
kittiphong2 <68525496+kittiphong2@users.noreply.github.com>
2020-08-21 01:12:08 +0700

    bt

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="press a button" />
+    <hr />
+    <div class="row">
+      <div class="col-6">
+        <img alt="Vue logo" src=".\assets\img\1.jpg" width="350" />
+        <h1>{{total1}}</h1>
+      </div>
+      <div class="col-6">
+        <img alt="Vue logo" src=".\assets\img\2.jpg" width="300px" />
+        <h1>{{total2}}</h1>
+      </div>
+      <div class="col-6">
+        <CSButton @cc="sum" />
+      </div>
+      <div class="col-6">
+        <CSButton @cc="sum1" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import CSButton from "./components/csbutton";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    CSButton,
+  },
+
+  data: function () {
+    return {
+      total1: 0,
+      total2: 0,
+    };
+  },
+
+  methods: {
+    sum(counterValue) {
+      this.total1 = counterValue;
+    },
+    sum1(counterValue) {
+      this.total2 = counterValue;
+    },
+  },
+};
 </script>
 
 <style>
@@ -22,7 +56,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #8a8686;
+  margin-top: 50px;
 }
 </style>
---
+++ src/components/csbutton.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <button v-on:click="upFunction" class="btn btn-primary">LIKE </button>
+    <button v-on:click="downFunction" class="btn btn-success ml-1">LOVE </button>
+    <button v-on:click="LoveFunction" class="btn btn-danger ml-1">HATE </button>
+    <button v-on:click="ResetFunction" class="btn btn-warning ml-1">RESET </button>
+  </div>
+</template>
+<script>
+export default {
+  data: function () {
+    return {
+      counter: 0,
+    };
+  },
+  props: {
+    lable: String,
+  },
+  methods: {
+    upFunction: function () {
+      this.counter += 1;
+      this.$emit("cc", this.counter);
+    },
+
+    downFunction: function () {
+      this.counter += 5;
+      this.$emit("cc", this.counter);
+    },
+
+    LoveFunction: function () {
+      this.counter -= 10;
+      this.$emit("cc", this.counter);
+    },
+
+    ResetFunction: function () {
+      this.counter = 0;
+      this.$emit("cc", this.counter);
+    },
+  },
+};
+</script>
+<style>
+</style>
